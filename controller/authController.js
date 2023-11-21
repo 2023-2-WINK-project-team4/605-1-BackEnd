50770c5d29982bd9ea15ffb19752187b93ba2c03
U-Geon <sksmsfbrjs@naver.com>
2023-11-21 22:06:37 +0900

    [FEAT]: add /main router

--- controller/authController.js
+++ controller/authController.js
@@ -1,23 +1,26 @@
+const Member = require('../models/member');
+
+// 회원 가입 로직
 exports.join = async (req, res) => {
     try {
         // 받은 값으로 회원 가입 완료.
-        Member.updateOne( { kakaoId: req.body.kakaoId }, {
+        await Member.updateOne({ kakaoId: req.body.kakaoId }, {
             $set: {
                 name: req.body.name,
                 studentId: req.body.studentId,
                 club: req.body.club,
                 profile: req.body.profile,
             }
-        })
+        });
         res.status(200).json({
-            "message": "회원 가입 성공",
-            "redirect": "/main"
-        }); // 회원 가입 성공 시 메인으로 리다이렉트
+            msg: '회원 가입 성공',
+            redirect: '/main'
+        });
     } catch (error) {
         console.error(error);
         res.status(400).json({
-            "message": error.message
-        }); // 오류 발생 시 첫 화면으로 redirect
+            message: error.message,
+            redirect: '첫 화면'
+        });
     }
-}
-
+};
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express')
-const {home} = require('../controller/homeController')
+const {home} = require('../controller/mainController')
 const router = express.Router()
 
 
-router.get('/main', home)
+router.get('/main', home) // 홈 화면
 
 module.exports = router;
