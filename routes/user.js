6ec93efe03c3e02c5a286a8c97a4fe51da0f41dc
didskfo <didskfo2003@gmail.com>
2023-11-21 22:20:51 +0900

    chore. multer수정

--- routes/user.js
+++ routes/user.js
@@ -4,10 +4,6 @@ const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
 
-const storage = multer({
-    dest: 'uploads/'
-})
-
 const router = express.Router();
 
 try {
