f167f2cc65fcbe5108394d8a43df2cd5bab85a32
SuperH0ng <ghdtmdgus100@gmail.com>
2023-11-30 05:37:11 +0900

    feat : 좌석 반납 구현 (반납시 history 기록)

--- controller/seatController.js
+++ controller/seatController.js
@@ -31,6 +31,7 @@ exports.checkMySeat = async (req, res) => {
         });
     }
 }
+
 // 좌석 대여
 exports.rentSeat = async (req, res) => {
     const { seatNumber } = req.body;
@@ -68,7 +69,50 @@ exports.rentSeat = async (req, res) => {
 
 // 좌석 반납
 exports.returnSeat = async (req, res) => {
+    const memberId = req.memberId;
 
+    try {
+        // 현재 인증된 사용자의 정보를 조회
+        const member = await Member.findById(memberId);
+        if (!member) {
+            return res.status(404).json({ message: '회원을 찾을 수 없습니다.' });
+        }
+
+        // 해당 좌석을 조회
+        const seat = await Seat.findOne({ memberId: memberId });
+        if (!seat) {
+            return res.status(404).json({ message: '좌석을 찾을 수 없습니다.' });
+        }
+
+        // 좌석이 이미 사용 중인 경우
+        if (seat.status === 'notUsed') {
+            return res.status(400).json({ message: '이미 반납된 좌석입니다.' });
+        }
+
+        const seatHistory = new SeatHistory({
+            memberName : member.name,
+            studentId : member.studentId,
+            club : member.club,
+            seatNumber : seat.number,
+            seatStartTime : seat.startTime,
+            seatEndTime : Date.now(),
+            memberId : memberId
+        });
+
+        console.log("seat 찾았니?")
+
+        await seatHistory.save();
+
+        // 좌석 상태 업데이트
+        seat.status = 'notUsed';
+        seat.startTime = null;
+        seat.memberId = null;
+        await seat.save();
+
+        res.status(200).json({ message: '좌석 반납 완료' });
+    } catch (error) {
+        res.status(500).json({ message: '서버 오류' });
+    }
 };
 
 // 좌석 조회
