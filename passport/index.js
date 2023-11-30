0d841c2e99e7cfaeee6db44c3a08e9f2aee0ba78
U-Geon <sksmsfbrjs@naver.com>
2023-11-30 21:40:24 +0900

    fix: update session key value

--- passport/index.js
+++ passport/index.js
@@ -5,15 +5,14 @@ const kakao = require('./kakaoStrategy');
 
 module.exports = () => {
     passport.serializeUser((user, done) => { // 로그인 시 session 저장
-        done(null, user._id);
+        done(null, user.kakaoId);
     });
 
     passport.deserializeUser((id, done) => { // 로그인 시 세션 불러오기. req.user로 접근 가능!
-        Member.findOne({ _id: id })
+        Member.findOne({ kakaoId: id })
             .then(member => done(null, member))
             .catch(err => done(err));
     });
 
     kakao();
 }
-
