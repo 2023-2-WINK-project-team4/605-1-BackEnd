68d3875d77b7b07ed24575e5f274a342e6edfe6b
U-Geon <sksmsfbrjs@naver.com>
2024-01-10 03:18:34 +0900

    fix: update Club name

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const Seat = require("./seat");
-const clubs = ['wink', 'foscar'];
+const clubs = ['WINK', 'FOSCAR'];
 
 const connect = () => {
     // 1. 개발 환경일 떄만 콘솔을 통해 몽구스가 생성하는 쿼리 내용을 확인할 수 있게 하는 부분
