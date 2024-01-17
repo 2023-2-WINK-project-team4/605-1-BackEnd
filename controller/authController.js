96d1c219477fa881053e1443c85092b268590bdc
U-Geon <sksmsfbrjs@naver.com>
2024-01-17 13:06:26 +0900

    fix: join controller

--- controller/authController.js
+++ controller/authController.js
@@ -5,7 +5,7 @@ const Member = require('../models/member');
 exports.join = async (req, res) => {
     try {
         // 받은 값으로 회원 가입 완료.
-        await Member.updateOne({ _id: req.header.id }, {
+        await Member.updateOne({ _id: req.header.authorization }, {
             $set: {
                 name: req.body.name,
                 studentId: req.body.studentId,
