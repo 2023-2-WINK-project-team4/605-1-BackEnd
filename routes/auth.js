2033d17354ed42f745e88853f921a512e523e5c2
U-Geon <sksmsfbrjs@naver.com>
2024-03-02 00:06:49 +0900

    fix: auto login

--- routes/auth.js
+++ routes/auth.js
@@ -20,13 +20,7 @@ authRouter.get("/login", (req, res, next) => {
             token : token
         })
     } catch(error) {
-        if (error.name === 'TokenExpireError') {
-            return next();
-        }
-        return res.status(401).json({
-            code: 401,
-            message: '유효하지 않은 토큰입니다.'
-        });
+        return next();
     }
 }, passport.authenticate("kakao"));
 
